954ff9c4262a392668e62a1be0c02f997b1389e5
moustafa ascoura <moustafaascoura@gmail.com>
2023-10-17 23:07:28 +0300

    add info

--- CrowdFunder/CrowdFunder/settings.py
+++ CrowdFunder/CrowdFunder/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'CrowdFunder.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default' : {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'croundfunder',
+        'USER':'django_crowd',
+        'PORT':'5432',
+        'HOST':'localhost',
+        'PASSWORD':'abc123'
     }
 }
 
