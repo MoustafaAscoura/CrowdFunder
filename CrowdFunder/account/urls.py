4f1de5d9d22c6f5860aea6366faf112978958a75
moustafa ascoura <moustafaascoura@gmail.com>
2023-10-20 01:57:35 +0300

    User Activation by mail

--- CrowdFunder/account/urls.py
+++ CrowdFunder/account/urls.py
@@ -1,14 +1,12 @@
-from django.urls import path,include
+from django.urls import path,include,re_path
 from .views import *
 
 urlpatterns = [
     path('login/', Login.as_view(), name='login'),
     path('signup/', CreateAccount.as_view(), name='signup'),
     path('success/', Success, name='success'),
-    path('token' , token_send , name='token_send'),
-    path('error' , error_page , name='error'),
-    path('verify/<auth_token>' , verify ,name = "verify"),
-    
+    path('activate/<uidb64>/<token>/', activate, name='activate'),
+    path('resend_mail/<str:uname>', ResendMail, name="resend_mail"),
     path('profile/<int:pk>', ShowProfile.as_view(), name='profile'),
     path('editprofile/<int:pk>', EditAccount.as_view(), name='editprofile'),
     path('profile/delete/<int:pk>', DeleteAccount.as_view(), name='delete_user'),
--- CrowdFunder/account/views.py
+++ CrowdFunder/account/views.py
@@ -1,16 +1,17 @@
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, logout
 from django.contrib.auth.views import LoginView
-from django.conf import settings
-from django.core.mail import send_mail
 from django.shortcuts import render,redirect,resolve_url
 from django.urls import reverse_lazy,reverse
+from django.utils.encoding import force_str
+from django.utils.http import urlsafe_base64_decode
 from django.views import generic
 
 from .models import User
-from .forms import FullUserForm,CreateUserForm
+from .forms import *
+from .tokens import account_activation_token,send_verification_email
 
 class Login(LoginView):
+    form_class = LoginForm
     redirect_authenticated_user=True
     def get_default_redirect_url(self):
         if self.next_page:
@@ -20,63 +21,51 @@ class Login(LoginView):
 class CreateAccount(generic.CreateView):
     model = User
     form_class = CreateUserForm
-    success_url = reverse_lazy('token_send')
+    success_url = reverse_lazy('success')
     template_name='registration/signup.html'
     def form_valid(self, form):
-        form.save()
-        username = self.request.POST['username']
-        password = self.request.POST['password1']
-        user = authenticate(username=username, password=password)
-        login(self.request, user)
+        user = form.save(commit=False)
+        user.is_active = False
+        user.save()
+        send_verification_email(self.request,user)
         return redirect(self.success_url)
 
 def Success(request):
     return render(request, 'registration/success.html')
 
-def token_send(request):
-    return render(request, 'registration/token.html')
-
-def verify(request , auth_token):
+def activate(request, uidb64, token):
     try:
-        user = User.objects.filter(auth_token = auth_token).first()
-        if user:
-            user.is_verified = True
-            user.save()
-            return redirect('login')
-        else:
-            return redirect('error')
-    except Exception as e:
-        print(e)
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+        return redirect(reverse('index'))
+    else:
+        return redirect(reverse('error'))
 
-
-def error_page(request):
-    return render(request, 'registration/error.html')   
-
-
-def send_mail_after_registration(email,token):
-    subject = "Your account needs to be verified"
-    message = f'Hi paste your link to verify your account http://127.0.0.1:8000/verify/{token}'
-    email_from = settings.EMAIL_HOST_USER
-    recipient_list = [email]
-    send_mail(subject, message , email_from ,recipient_list)
+def ResendMail(request,uname):
+    user = User.objects.get(username=uname)
+    send_verification_email(request,user)
+    return redirect('success')
 
 class ShowProfile(generic.DetailView):
     model = User
     template_name = 'auth/profile.html'
     context_object_name = 'profileuser'
 
+def Logout(request):
+    logout(request)
+    return redirect('index')
 
 class EditAccount(generic.UpdateView):
     model = User
     form_class = FullUserForm
     success_url = reverse_lazy('home')
     template_name='registration/edit.html'
-
-
-
-def Logout(request):
-    logout(request)
-    return redirect('index')
 
 class DeleteAccount(generic.DeleteView):
     model = User
