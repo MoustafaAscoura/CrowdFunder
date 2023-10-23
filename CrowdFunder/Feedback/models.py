4e077827ce2ec93056bfc0f17e2771658aea98df
osman122 <osoconan1@gmail.com.com>
2023-10-23 15:57:29 +0300

    add models fields

--- CrowdFunder/Feedback/models.py
+++ CrowdFunder/Feedback/models.py
@@ -1,6 +1,31 @@
 from django.db import models
+from projects.models import Project 
+from account.models import User
+
+
 
 # Create your models here.
+class Comment(models.Model):
+    cotent=models.CharField(max_length=500)
+    project = models.ForeignKey(Project,on_delete=models.CASCADE)
+    user =models.ForeignKey(User,on_delete=models.CASCADE)
+
+
+
+
+class Review(models.Model):
+    review=models.CharField(max_length=500)
+    project = models.ForeignKey(Project,on_delete=models.CASCADE)
+    user =models.ForeignKey(User,on_delete=models.CASCADE)
+
+
+class Report(models.Model):
+    reason=models.CharField(max_length=500)
+    statues=models.CharField(max_length=5)
+    project = models.ForeignKey(Project,on_delete=models.CASCADE)
+    comment =models.ForeignKey(Comment,on_delete=models.CASCADE)
+    user =models.ForeignKey(User,on_delete=models.CASCADE)
+
 
 
 #Ya osman, remember to user on_delete = cascade on all your models.
