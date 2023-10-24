1f15850efddf3be71621d670920c8e314a616be3
omar-syed <omarrrrr118@gmail.com>
2023-10-24 19:39:37 +0300

    fix edit project using class based views

--- CrowdFunder/projects/views.py
+++ CrowdFunder/projects/views.py
@@ -5,6 +5,11 @@ from . forms import ProjectForm
 from django.contrib.auth.decorators import login_required
 from django.views import generic
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import UpdateView
+from django.core.exceptions import PermissionDenied
+
+
 
 def project_list(request):
     projects = Project.objects.all()
@@ -41,44 +46,58 @@ class CreateProject(generic.CreateView):
         self.object.save()
         return super().form_valid(form)
 
-@login_required
-def edit_project(request, id):
-    project = get_object_or_404(Project, id=id)
+class EditProjectView(LoginRequiredMixin, UpdateView):
+    model = Project
+    form_class = ProjectForm
+    template_name = 'projects/create/edit.html'
+    success_url = '/project_list/'
+
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset=queryset)
+        if obj.user != self.request.user:
+            raise PermissionDenied("You are not authorized to edit this project.")
+        return obj
+
+# @login_required
+# def edit_project(request, id):
+#     project = get_object_or_404(Project, id=id)
     
-    # Check if the current user is the owner of the gun
-    if project.user != request.user:
-        return HttpResponse("You are not authorized to edit this project.")
+#     # Check if the current user is the owner of the gun
+#     if project.user != request.user:
+#         return HttpResponse("You are not authorized to edit this project.")
 
-    if request.method == 'POST':
-        form = ProjectForm(request.POST, request.FILES)
-        if form.is_valid():
-            project.title = form.cleaned_data['title']
-            project.details = form.cleaned_data['details']
-            project.total_target = form.cleaned_data['total_target']
-            project.start_time = form.cleaned_data['start_time']
-            project.end_time = form.cleaned_data['end_time']
-            project.category = form.cleaned_data['category']
+#     if request.method == 'POST':
+#         form = ProjectForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             project.title = form.cleaned_data['title']
+#             project.details = form.cleaned_data['details']
+#             project.total_target = form.cleaned_data['total_target']
+#             project.start_time = form.cleaned_data['start_time']
+#             project.end_time = form.cleaned_data['end_time']
+#             project.category = form.cleaned_data['category']
+#             project.tags = form.cleaned_data['tags']
 
-            # Check if a new image is provided
-            # if 'image' in request.FILES:
-            #     project.image = form.cleaned_data['image']
-            project.save()
-            return redirect('project_list')
-    else:
-        form = ProjectForm({
-            'title': project.title,
-            'details': project.details,
-            'total_target': project.total_target,
-            'start_time': project.start_time,
-            'end_time': project.end_time,
-            'category': project.category,
-            }
-        )
+#             # Check if a new image is provided
+#             # if 'image' in request.FILES:
+#             #     project.image = form.cleaned_data['image']
+#             project.save()
+#             return redirect('project_list')
+#     else:
+#         form = ProjectForm({
+#             'title': project.title,
+#             'details': project.details,
+#             'total_target': project.total_target,
+#             'start_time': project.start_time,
+#             'end_time': project.end_time,
+#             'category': project.category,
+#             'tags': project.tags,
+#             }
+#         )
 
-    context = {
-        'form': form
-    }
-    return render(request, 'projects/create/edit.html', context)
+#     context = {
+#         'form': form
+#     }
+#     return render(request, 'projects/create/edit.html', context)
 
 class CategoryView(generic.ListView):
     template_name = 'projects/project_list.html'
